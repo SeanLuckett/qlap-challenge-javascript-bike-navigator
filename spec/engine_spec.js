be95b6a00af12eb1faa4f444e2cec80d232491b4
Sean <sean@quickleft.com>
2012-08-07 17:52:55 -0600

    Gives attributes to Bike.

--- spec/engine_spec.js
+++ spec/engine_spec.js
@@ -12,4 +12,14 @@ describe("Bike", function() {
   it("has 2 wheels", function(){
     expect(bike.wheels).toEqual(2);
   });
+
+  it("has dimensions", function(){
+    expect(bike.height).toEqual(4);
+    expect(bike.width).toEqual(6);
+  });
+
+  it("has a position", function(){
+    expect(bike.lat).toEqual(40.017094);
+    expect(bike.lng).toEqual(-105.283799);
+  });
 });
--- src/engine.js
+++ src/engine.js
@@ -1,2 +1,11 @@
 function Bike(){
 }
+
+Bike.prototype.wheels = 2;
+
+Bike.prototype.height = 4;
+Bike.prototype.width = 6;
+
+Bike.prototype.lat = 40.017094;
+Bike.prototype.lng = -105.283799;
+
