65017a065c27b3790534072aa0b1e82f57ae9fc5
Sean <sean@quickleft.com>
2012-08-09 15:41:36 -0600

    Adds bike class and specs. All green

--- spec/engine_spec.js
+++ spec/engine_spec.js
@@ -25,17 +25,62 @@ describe("Bike", function() {
 
   describe("#move", function(){
 
+    it("throws an error when called with no arguments", function(){
+      expect(bike.move).toThrow("Wrong number of arguments: 0 for 2");
+    });
+
+    it("throws an error when called with one argement", function(){
+      expect(bike.move.bind(this, "south")).toThrow("Wrong number of arguments: 1 for 2");
+    });
+
+    describe("latitude", function(){
+
+      it("adds to latitude when going north", function(){
+        moveNorth = bike.move("north", 20.5);
+        expect(moveNorth).toEqual([40.313457, bike.lng]);
+      });
+
+      it("subtracts from latitude when going south", function(){
+        moveSouth = bike.move("south", 20.5);
+        expect(moveSouth).toEqual([39.720731, bike.lng]);
+     });
+    });
+
+    describe("longititude", function(){
+
+      it("adds to longitude when going east", function() {
+        moveEast = bike.move("east", 20.5);
+        expect(moveEast).toEqual([bike.lat, -104.987436]);
+      });
+
+      it ("subtracts from longitude when going west", function() {
+        moveWest = bike.move("west", 20.5);
+        expect(moveWest).toEqual([bike.lat, -105.580162]);
+      });
+    });
+  });
+
+  describe("#reset", function(){
     beforeEach(function(){
-      spyOn(bike, 'move');
-      newLoc = bike.move("north", 2.5);
+      bike.lat = 0;
+      bike.lng = 0;
+      defaultPosition = bike.reset();
     });
 
-    it("gets called", function(){
-      expect(bike.move).toHaveBeenCalledWith("north", 2.5);
+    it("resets bike's position to default", function () {
+      expect(bike.lat).toEqual(40.017094);
+      expect(bike.lng).toEqual(-105.283799);
     });
 
-    xit("returns and array", function(){
-      // May just check that the return type is an array or something; not sure.
+    it("returns an array including lat and lng", function(){
+      expect(defaultPosition).toEqual([40.017094, -105.283799]);
+    });
+  });
+
+  describe("#plotLocation", function(){
+    it("returns location as an array", function(){
+      pos = bike.plotLocation();
+      expect(pos).toEqual([40.017094, -105.283799]);
     });
   });
 });
--- src/engine.js
+++ src/engine.js
@@ -1,13 +1,48 @@
 function Bike(){
+  this.wheels = 2;
+  this.height = 4;
+  this.width = 6
+
+  this.lat = 40.017094;
+  this.lng = -105.283799;
+
 }
 
-Bike.prototype.wheels = 2;
+Bike.prototype.move = function(direction, distance){
+  if (arguments.length < 2) throw "Wrong number of arguments: " + arguments.length + " for 2"
 
-Bike.prototype.height = 4;
-Bike.prototype.width = 6;
+  var conversion = 69.172;
+  var numDegrees = distance / conversion;
 
-Bike.prototype.lat = 40.017094;
-Bike.prototype.lng = -105.283799;
+  switch(direction) {
+    case "north":
+      this.lat += parseFloat(numDegrees.toFixed(6));
+      break;
+    case "south":
+      this.lat -= parseFloat(numDegrees.toFixed(6));
+      break;
+    case "east":
+      this.lng += parseFloat(numDegrees.toFixed(6));
+      break;
+    case "west":
+      this.lng -= parseFloat(numDegrees.toFixed(6));
+      break;
+    default:
+      throw "direction incorrectly entered";
+      break;
+  }
 
-Bike.prototype.move = function(){
+  return [this.lat, this.lng];
+};
+
+Bike.prototype.reset = function(){
+  var tempBike = new Bike();
+  this.lat = tempBike.lat;
+  this.lng = tempBike.lng;
+
+  return [this.lat, this.lng];
+};
+
+Bike.prototype.plotLocation = function(){
+  return [this.lat, this.lng];
 };
